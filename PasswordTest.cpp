3213cf5a1259b83a514368a965fea90cc6593277
Tiggzzz <abramjimenez2022@gmail.com>
2025-11-19 11:50:46 -0800

    Add unit tests for count_leading_characters

--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -20,3 +20,24 @@ TEST(PasswordTest, single_letter_password)
 	int actual = my_password.count_leading_characters("Z");
 	ASSERT_EQ(1, actual);
 }
+
+TEST(PasswordTest, diff_case_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("zZ");
+	ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, empty_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("");
+	ASSERT_EQ(0, actual);
+}
+
+TEST(PasswordTest, triple_letter_password)
+{
+	Password my_password;
+	int actual = my_password.count_leading_characters("ZZZz");
+	ASSERT_EQ(3, actual);
+}
